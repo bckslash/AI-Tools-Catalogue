d1b8e285bcef85dd5051fca5b881b004f73212e1
Robert Smrek <65281063+bckslash@users.noreply.github.com>
2024-05-17 12:58:11 +0200

    Update Footer.jsx

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
 	return (
 		<footer className="inset-x-0 bottom-0 text-center py-2 mt-16 opacity-30 text-xs">
 			<a
-				href="https://github.com/bckslash"
+				href="https://github.com/bckslash/AI-Tools-Catalogue"
 				target="_blank"
 				rel="noopener noreferrer"
 			>
